4cc1e01407ed84542d806d76c00a204c6f60330c
Sohel Rana <srana@leapinglogic.com>
2019-01-30 18:42:47 +0600

    map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { MapsAPILoader } from '@agm/core';
 
+declare const google: any;
 
 @Component({
   selector: 'app-root',
@@ -8,102 +8,49 @@ import { MapsAPILoader } from '@agm/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  // title = 'angualr-map';
-  // lat: number = 23.7998;
-  // lng: number = 90.352;
+   ngOnInit() {
+      this.initMap();
+   }
 
-  public latitude: number;
-  public longitude: number;
-  public maxSpeed: number;
-  public zoom: number;
-  public polyline: Array<any>;
-  public polylines: Array<any>;
-  
-  
-  constructor(
-    private mapsAPILoader: MapsAPILoader
-  ) {}
-  
-  ngOnInit() {
-    //set google maps defaults
-    this.zoom = 3;
-    this.maxSpeed = 40;
-    this.latitude = 21.291;
-    this.longitude = -122.214;
+  initMap() {
 
-    this.polyline = [
-        {
-            latitude:   23.7998,
-            longitude: 90.352,
-            speed: 50
-        },
-         {
-            latitude:  23.746466,
-            longitude: 90.376015,
-            speed: 50
-         }
-        // },
-        // {
-        //     latitude: 37.772,
-        //     longitude: -122.214,
-        //     speed: 20
-        // },
-        // {
-        //     latitude: 21.291,
-        //     longitude: -157.821,
-        //      speed: 20
-        // },
-        // {
-        //     latitude: -18.142,
-        //     longitude: 178.431,
-        //     speed: 20
-        // },
-        // {
-        //     latitude: -27.467,
-        //     longitude: 153.027,
-        //     speed: 25
-        // }
-    ]
-    this.polylines = this.rebuildPolylines();
- 
-    
-    //set current position
-    //this.setCurrentPosition();
-    
-    //load Places Autocomplete
-    this.mapsAPILoader.load().then(() => {
+    let marker;
+    const mirpur = {lat:23.7998, lng: 90.352};
+    const dhanmondi = {lat:23.746466, lng: 90.376015};
 
+    let map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 12,
+      center: {lat: 23.777176, lng: 90.399452},
+      mapTypeId: 'terrain'
+    });
+
+    let flightPlanCoordinates = [
+      {lat: 23.7998, lng: 90.352},
+      {lat: 23.746466, lng: 90.376015},
+      {lat: 23.759739, lng: 90.392418},
+      {lat: 23.797911, lng: 90.414391},
+    ];
+    let flightPath = new google.maps.Polyline({
+      path: flightPlanCoordinates,
+      geodesic: true,
+      strokeColor: '#000000',
+      strokeOpacity: 1.0,
+      strokeWeight: 4
+    });
+
+    flightPath.setMap(map);
+
+    marker = new google.maps.Marker({
+      position: mirpur,
+      map: map,
+      title: 'Mirpur-12'
+    });
+    marker = new google.maps.Marker({
+      position: dhanmondi,
+      map: map,
+      title: 'Dhanmondi'
     });
   }
-  private rebuildPolylines() {
-    let polylines = [];
-    let i = 0;
-    let newPolyline = {path: [], color: 'blue'};
-    for (let point of this.polyline) {
-      console.log(point)
-      newPolyline.path.push(point);
-      const speedChanged = this.polyline[i+1] && (point.speed < this.maxSpeed && this.polyline[i+1].speed < this.maxSpeed) ||(point.speed > this.maxSpeed && this.polyline[i+1].speed > this.maxSpeed )
-      if (point.speed > this.maxSpeed) {
-        newPolyline.color = 'red';
-      }
-      if (speedChanged) {
-        newPolyline.path.push( this.polyline[i+1] );
-        polylines.push(newPolyline);
-        newPolyline = {path: [], color: 'blue'};
-      }
-      i++;
-    }
-    console.log(polylines);
-    return polylines;
-    
-  }
-  private setCurrentPosition() {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        this.latitude = position.coords.latitude;
-        this.longitude = position.coords.longitude;
-        this.zoom = 12;
-      });
-    }
-  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, ElementRef, ViewChild  } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { AgmCoreModule } from '@agm/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,13 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
     AppRoutingModule,
-    AgmCoreModule.forRoot({
-      apiKey: '',
-      libraries: ["places"]
-    })
   ],
   providers: [],
   bootstrap: [AppComponent]
